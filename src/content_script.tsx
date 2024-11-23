fdc6bddfa0bc18ee19300bf7cdee39ed8083c9e5
Melchior Lozé <melchior.loze@gmail.com>
2024-11-24 00:11:42 +0100

    add mute/unmute logic

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,46 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
+const getMuteButton = (): HTMLButtonElement => {
+  const muteButton = document.body.querySelector<HTMLButtonElement>(
+    'button[data-a-target="player-mute-unmute-button"]',
+  );
+  if (!muteButton) throw new Error('Mute button not found');
+  return muteButton;
+};
+
+const isPlayerMuted = () => {
+  const muteButton = getMuteButton();
+  return muteButton.getAttribute('aria-label')?.startsWith('Unmute');
+};
+
+const mutePlayer = () => {
+  const muteButton = getMuteButton();
+  if (!isPlayerMuted()) muteButton.click();
+};
+
+const unmutePlayer = () => {
+  const muteButton = getMuteButton();
+  if (isPlayerMuted()) muteButton.click();
+};
+
+const isNodeAdIndicator = (node: Node) => {
+  return true; // TODO: Add condition to check if an ad was added
+};
+
+const observer = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    if (mutation.type === 'childList') {
+      mutation.addedNodes.forEach(node => {
+        if (isNodeAdIndicator(node)) mutePlayer();
+      });
+      mutation.removedNodes.forEach(node => {
+        if (isNodeAdIndicator(node)) unmutePlayer();
+      });
+    }
+  });
 });
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
+
+console.log('Mute Twitch Ads content script loaded');
